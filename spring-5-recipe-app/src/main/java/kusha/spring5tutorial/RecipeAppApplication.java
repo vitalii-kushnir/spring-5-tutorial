495f5f7221821eac372a17e9e220b3578d9537a4
Vitalii Kushnir <kusha.apple@gmail.com>
2017-11-06 20:32:48 +0100

    One To One JPA Relationships

---
+++ spring-5-recipe-app/src/main/java/kusha/spring5tutorial/RecipeAppApplication.java
@@ -0,0 +1,12 @@
+package kusha.spring5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppApplication.class, args);
+	}
+}
