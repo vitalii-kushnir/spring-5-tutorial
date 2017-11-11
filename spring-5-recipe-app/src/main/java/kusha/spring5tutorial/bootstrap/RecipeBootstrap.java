dcc2b26be5fa0d6c0bf72c3fef363466ec96c046
Vitalii Kushnir <kusha.apple@gmail.com>
2017-11-11 14:08:32 +0100

    Creating a Command Object and Type Conversions in Spring

--- spring-5-recipe-app/src/main/java/kusha/spring5tutorial/bootstrap/RecipeBootstrap.java
+++ spring-5-recipe-app/src/main/java/kusha/spring5tutorial/bootstrap/RecipeBootstrap.java
@@ -20,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Created by jt on 6/13/17.
+ */
 @Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
@@ -41,7 +44,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
-        log.debug("Loadnig bootstrap data...");
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes() {
@@ -167,6 +170,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
+
         //add to return list
         recipes.add(guacRecipe);
 
@@ -232,6 +239,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
+
+        tacosRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacosRecipe.setServings(4);
+        tacosRecipe.setSource("Simply Recipes");
 
         recipes.add(tacosRecipe);
         return recipes;
--- spring-5-recipe-app/src/main/java/kusha/spring5tutorial/service/RecipeService.java
+++ spring-5-recipe-app/src/main/java/kusha/spring5tutorial/service/RecipeService.java
@@ -1,5 +1,6 @@
 package kusha.spring5tutorial.service;
 
+import kusha.spring5tutorial.commands.RecipeCommand;
 import kusha.spring5tutorial.domain.Recipe;
 
 import java.util.Set;
@@ -9,4 +10,6 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
 
     Recipe findById(Long id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
